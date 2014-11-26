713f3301ba44ad5cef239658998ca970d62feb97
eiriks0 <cam-bridge@hotmail.com>
2014-11-26 16:35:11 +0100

    ny

---
+++ endre.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <link href="css/main.css" rel="stylesheet">
+   <meta charset="utf-8">
+   </head>
+   <body>
+   
+  <div class="container">
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="./">Tilbake</a>
+                    </li>
+                    <li>
+                        <a href="register-data.php">Registrer data</a>
+                    </li>
+                    <li class="endre.php">
+                        <a href="endre.php">Endre data</a>
+                    </li>
+                    <li class="dashed">
+                        <a href="">Slett data</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+		  <form>
+<h2 class="text-blue">Gjør endringer</h2>
+
+<br>
+<input type="radio" name="valg" value="student" onclick="window.open('endre-student.php')" /> Endre student
+<br>
+
+<br>
+<input type="radio" name="valg" value="klasse" onclick="window.open('endre-klasse.php')" /> Endre klasse
+<br>
+</form>
+
+
+        </div>
+ 
+
+   </body>
+</html>
